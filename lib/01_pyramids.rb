d576f0815f3d91ced206857e9ed199acb76e3217
Laspargus <maxime.speroni@gmail.com>
2019-01-10 12:02:46 +0100

    ajout wtc_pyramide

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -33,6 +33,9 @@ end
 #half_pyramid 
 
 
+
+
+
 #PYRAMIDE DE GIZEH
 
 
@@ -71,6 +74,11 @@ end
 
 #full_pyramid
 
+
+
+
+
+
 #ALEXANDRIE ALEXANDRA
 
 
@@ -79,30 +87,52 @@ def wtf_pyramid
 		puts "Salut bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 	print ">"
 
+
+
 	number_of_floors = gets.to_i 
 	# fonctionne également avec gets.chomp.to_i
 
-	half_floors = number_of_floors/2
 
+#condition pour tester sir le nombre d'étage est pair
+if number_of_floors.even? 
+	puts "Ce jeu ne fonctionne pas avec les nombres pairs"
+
+else
+
+	half_floors = (number_of_floors/2)+1
+	floor_under_half=half_floors -1
+
+	#je teste ma variable half_floors
 	puts half_floors
+
+	#je teste ma variable floor_under_half
+	puts floor_under_half
 
 	puts "Voici la pyramide :"
 
+		#Premiere moitié de pyramide
+		half_floors.times do |i|
+				blankspace = " " * (number_of_floors - (i + 1))
 
-		number_of_floors.times do |i|
-
-
-			blankspace = " " * (number_of_floors - (i + 1))
-
-			number_hastag = "#" * (i + 1)
+				number_hastag = "#" * (i + 1)
 			
-			hastagsup = "#" * i
+				hastagsup = "#" * i
 			
-			puts "#{blankspace}#{number_hastag}#{hastagsup}"
+				puts "#{blankspace}#{number_hastag}#{hastagsup}"
+		end
 
-			
-	
-	end
+		#Deuxième moitié de pyramide
+		floor_under_half.times do |i|
+		
+				blankspace = " " * (half_floors + i)
+		
+				number_hastag = "#" * (number_of_floors-i-(2+i))
+		
+				puts "#{blankspace}#{number_hastag}"
+		end
+
+
+end
 		
 
 end
