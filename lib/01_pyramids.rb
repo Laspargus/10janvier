3020d24210dfd0db50d1829bf48b761e24402608
lebonvieuxlion <debaugelion@gmail.com>
2019-01-10 10:56:55 +0100

    EXO 2 réalisé + début exo3

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,4 +1,5 @@
 
+#MOITIE DE PYRAMIDE
 
 def half_pyramid
 
@@ -29,11 +30,10 @@ end
 
 end
 
-#half_pyramid 
+half_pyramid 
 
 
-
-
+#PYRAMIDE DE GIZEH
 
 
 def full_pyramid
@@ -70,3 +70,39 @@ end
 end
 
 full_pyramid
+
+#ALEXANDRIE ALEXANDRA
+
+
+def wtf_pyramid
+
+		puts "Salut bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+	print ">"
+
+	number_of_floors = gets.to_i 
+	# fonctionne également avec gets.chomp.to_i
+
+
+	puts "Voici la pyramide :"
+
+	if number_of_floors < 26
+	number_of_floors.times do |i|
+		
+		
+
+		blankspace = " " * (number_of_floors - (i + 1))
+
+		number_hastag = "#" * (i + 1)
+		
+		hastagsup = "#" * i
+		
+		puts "#{blankspace}#{number_hastag}#{hastagsup}"
+		
+	end
+
+	else puts "Désolé mais une pyramide ne peut avoir que 25 étages maximum."
+		
+
+	end 
+
+end
